91d3c510baa40bf2dbc7e4597537611271281f10
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-07-20 16:45:46 -0300

    feat: add createUserWithOuth0 & updateUser

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,12 +1,25 @@
 import { User } from "../repositories";
+import { response } from "../utils";
 
 export const getUsers = async (req, res) => {
   const users = await User.list();
-  res.status(200).json(users);
+  response(res, 200, users);
 };
 
 export const createUser = async (req, res) => {
   const userToCreate = req.body;
   const newUser = await User.create(userToCreate);
-  res.status(201).json(newUser);
+  response(res, 201, newUser);
+};
+
+export const createUserWithOuth0 = async (req, res) => {
+  const userToCreate = req.body;
+  const newUser = await User.findOrCreate(userToCreate);
+  response(res, 201, newUser);
+};
+
+export const updateUser = async (req, res) => {
+  const userToUpdate = req.body;
+  const newUser = await User.update(userToUpdate);
+  response(res, 201, newUser);
 };
