f4c110bd74cf1f380b24f4788d8fe9746fa12919
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-07-20 10:38:08 -0300

    perf: catched controllers

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,11 +1,12 @@
 import Router from "express";
 // import { User } from "../controllers";
 import { User } from "../controllers";
+import catchedAsync from "../utils/catchedAsync";
 
 const router = Router();
 
-router.get("/", User.getUsers);
+router.get("/", catchedAsync(User.getUsers));
 
-router.post("/", User.createUser);
+router.post("/", catchedAsync(User.createUser));
 
 export default router;
