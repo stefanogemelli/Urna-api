bd75e3ce8bcc9d105203c5ece6eb83e125f22eba
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-07-20 10:26:39 -0300

    feat: add createUser method in userController

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -4,3 +4,9 @@ export const getUsers = async (req, res) => {
   const users = await User.list();
   res.status(200).json(users);
 };
+
+export const createUser = async (req, res) => {
+  const userToCreate = req.body;
+  const newUser = await User.create(userToCreate);
+  res.status(201).json(newUser);
+};
