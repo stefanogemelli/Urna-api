d05edbe115994ed75f48e95236bf80ee2e16e362
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-07-20 10:26:01 -0300

    refactor: import and export of synchronized model

--- src/database/index.ts
+++ src/database/index.ts
@@ -1,7 +1,6 @@
 import mongoose from "mongoose";
 import { MONGO_URI } from "./config/envs";
-import userSchema, { IUser, UserModel } from "./schemas/usersSchema";
 
-const conn = mongoose.createConnection(MONGO_URI);
+export const conn = mongoose.createConnection(MONGO_URI);
 
-export const User = conn.model<IUser, UserModel>("User", userSchema);
+export { User } from "./schemas/usersSchema";
