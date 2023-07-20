1ec33c5b63e5e511b0f5927c850dca49f43c02f0
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-07-20 16:50:12 -0300

    feat: add userValidation middlewares

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -1,12 +1,20 @@
 import Router from "express";
-// import { User } from "../controllers";
 import { User } from "../controllers";
-import catchedAsync from "../utils/catchedAsync";
+import { catchedAsync } from "../utils";
+import { userValidation } from "../middlewares/validations";
 
 const router = Router();
 
 router.get("/", catchedAsync(User.getUsers));
 
-router.post("/", catchedAsync(User.createUser));
+router.post("/", userValidation.register, catchedAsync(User.createUser));
+
+// LAS DE ABAJO TODAVIA NO LAS TESTIÉ
+
+router.post("/outh0", userValidation.registerOuth0, catchedAsync(User.createUserWithOuth0));
+
+// hay que averiguar si queremos guardar el id en front para enviarlo en esta peticion o usamos
+// el email que viene por body nomás para el update
+router.patch("/", userValidation.update, catchedAsync(User.updateUser));
 
 export default router;
