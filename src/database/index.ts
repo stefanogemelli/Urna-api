8aab70d5d8e56131786fa93bea2d5246cd5c668f
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-07-21 10:41:41 -0300

    refactor: add barrel in schemas

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,5 +2,3 @@ import mongoose from "mongoose";
 import { MONGO_URI } from "./config/envs";
 
 export const conn = mongoose.createConnection(MONGO_URI);
-
-export { User } from "./schemas/usersSchema";
