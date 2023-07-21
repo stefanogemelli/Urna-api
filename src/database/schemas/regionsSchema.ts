2ac6b4ce887d70b98161bf37dd301d85c3e5b9b1
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-07-21 11:37:16 -0300

    feat: add regionsSchema

---
+++ src/database/schemas/regionsSchema.ts
@@ -0,0 +1,46 @@
+import { Schema, Model } from "mongoose";
+import { v4 as uuidv4 } from "uuid";
+import { conn } from "..";
+
+export interface IRegion {
+  _id: string;
+  name: string;
+}
+export interface RegionModel extends Model<IRegion> {
+  list(): Array<IRegion>;
+  getByName(): IRegion;
+  findOrCreate(region: IRegion): IRegion;
+  update(region: IRegion): IRegion;
+}
+
+const regionSchema = new Schema<IRegion, RegionModel>(
+  {
+    _id: {
+      type: String,
+      default: () => uuidv4(),
+    },
+    name: { type: String, required: true, unique: true, minlength: 3, maxlength: 40 },
+  },
+  { timestamps: true }
+);
+
+regionSchema.statics.list = async function () {
+  return await this.find();
+};
+
+regionSchema.statics.getByName = async function (name) {
+  return await this.findOne({ name });
+};
+
+regionSchema.statics.findOrCreate = async function (regionToCreate) {
+  const region = await this.findOne({ email: regionToCreate.email });
+  if (region) return region;
+  return await Region.create(regionToCreate);
+};
+
+regionSchema.statics.update = async function (regionToUpdate) {
+  const query = { _id: regionToUpdate._id };
+  return await this.findOneAndUpdate(query, regionToUpdate);
+};
+
+export const Region = conn.model<IRegion, RegionModel>("Region", regionSchema);
