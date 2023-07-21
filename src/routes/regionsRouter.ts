679ed075b70cca75129094083cac306228eb6221
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-07-21 11:36:45 -0300

    feat: add regionsRouter

---
+++ src/routes/regionsRouter.ts
@@ -0,0 +1,12 @@
+import Router from "express";
+import { Region } from "../controllers";
+import { catchedAsync } from "../utils";
+// import { regionValidation } from "../middlewares/validations";
+
+const router = Router();
+
+router.get("/", catchedAsync(Region.getRegions));
+
+router.post("/", /*regionValidation.create,*/ catchedAsync(Region.createRegion));
+
+export default router;
