eba73d4906733db99eaea56ad137178408a8b629
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-07-22 19:18:27 -0300

    perf: add user_id & description validators

--- src/middlewares/validations/validators.ts
+++ src/middlewares/validations/validators.ts
@@ -1,9 +1,19 @@
 import { ClientError } from "../../utils/errors";
+import { validate } from "uuid";
 
 export const allowPropertiesValidator = (objectData, allowProperties: Array<String>) => {
   if (Object.keys(objectData).some((prop) => !allowProperties.includes(prop)))
     throw new ClientError("Disallowed properties", 403);
 };
+
+export const idValidator = (_id: string) => {
+  try {
+    validate(_id);
+  } catch (error) {
+    throw new ClientError(error.message, 400);
+  }
+};
+
 export const emailValidator = (email: string) => {
   if (email.length >= 6 && email.length <= 255) return;
   throw new ClientError("The email must have between 6 and 255 characters", 400);
@@ -19,11 +29,15 @@ export const regionNameValidator = (name: string) => {
   throw new ClientError("The name must have between 3 and 40 characters", 400);
 };
 
-export const titleValidator = (name: string) => {
-  if (name.length <= 100) return;
+export const titleValidator = (title: string) => {
+  if (title.length <= 100) return;
   throw new ClientError("The title must have less than 100 characters", 400);
 };
 
+export const descriptionValidator = (description: string) => {
+  if (description.length <= 255) return;
+  throw new ClientError("The description must have less than 255 characters", 400);
+};
 export const dateValidator = (dateString: string) => {
   const date = Date.parse(dateString);
   if (isNaN(date)) throw new ClientError("Invalid date format", 400);
--- src/middlewares/validations/votingValidation.ts
+++ src/middlewares/validations/votingValidation.ts
@@ -1,10 +1,12 @@
 import { IVoting } from "../../database/schemas/votingSchema";
-import { dateValidator, titleValidator } from "./validators";
+import { dateValidator, titleValidator, idValidator, descriptionValidator } from "./validators";
 
 export const create = (req, res, next) => {
   const newVoting: IVoting = req.body;
 
+  idValidator(newVoting.user_id);
   titleValidator(newVoting.title);
+  descriptionValidator(newVoting.description);
   dateValidator(newVoting.opening_date);
   dateValidator(newVoting.closing_date);
 
