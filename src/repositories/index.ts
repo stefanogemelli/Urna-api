678ac8b05e10ca9b0549c3cdab13ff7acb300b97
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-07-22 22:43:34 -0300

    feat: add voteRepository

--- src/repositories/index.ts
+++ src/repositories/index.ts
@@ -2,3 +2,4 @@ export { UserRepository } from "./userRepository";
 export { RegionRepository } from "./regionRepository";
 export { VotingRepository } from "./votingRepository";
 export { OptionRepository } from "./optionRepository";
+export { VoteRepository } from "./voteRepository";
