fdd22faebac30979dca560a6db4bd5c0f550fcd2
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-07-24 09:12:13 -0300

    fix: delete method

--- src/repositories/voteRepository.ts
+++ src/repositories/voteRepository.ts
@@ -3,5 +3,4 @@ import { IVote } from "../database/schemas/votesSchema";
 
 export const VoteRepository = {
   create: async (vote: IVote): Promise<IVote> => Vote.create(vote),
-  delete: async (vote_id: string): Promise<IVote | {}> => Vote.delete(vote_id),
 };
