6a5ad16c9844f5c9f99561e388743a606d4714e4
FrancoFrias23 <lockke2323@gmail.com>
2023-07-25 11:04:01 -0300

    feat: add cookieparser midd

--- src/server.ts
+++ src/server.ts
@@ -2,11 +2,13 @@ import morgan from "morgan";
 import express from "express";
 import router from "./routes";
 import errorHandler from "./utils/errors/errorHandler";
+import cookieParser from "cookie-parser"
 
 const server = express();
 
 server.use(morgan("dev"));
 server.use(express.json());
+server.use(cookieParser());
 
 server.use(router);
 
