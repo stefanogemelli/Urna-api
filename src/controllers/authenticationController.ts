4932d1bdf637f7b03a84bba8ecbe8e604c093735
FrancoFrias23 <lockke2323@gmail.com>
2023-07-25 12:10:58 -0300

    feat: changes authentication controller

--- src/controllers/authenticationController.ts
+++ src/controllers/authenticationController.ts
@@ -7,14 +7,12 @@ export const getUser = async (req, res) => {
   const userDataFromAuth0 = req.oidc.user;
 
   const userData = await User.findByEmail(userDataFromAuth0.email);
-
+  
+  
   if (userData.length > 0) {
     const userInfo = JSON.parse(JSON.stringify(userData[0] ?? {}));
-
+    
     const token = jwt.sign({ email: userInfo.email, role: userInfo.role }, JWT_SECRET);
-    userInfo.token = token;
-
-    const queryParams = new URLSearchParams(userInfo).toString();
     
     res.cookie('token', token, { httpOnly: true, maxAge: 3600000 });
     res.cookie('userData', userInfo, { httpOnly: true, maxAge: 3600000 });
@@ -25,8 +23,7 @@ export const getUser = async (req, res) => {
       username: userDataFromAuth0.nickname,
       email: userDataFromAuth0.email,
     };
-    const queryParams = new URLSearchParams(userInfo).toString();
-
-    res.redirect(302, `${CLIENT_BASE_URL}/profile/complete?${queryParams}`);
+    res.cookie('userData', userInfo, { httpOnly: true, maxAge: 3600000 });
+    res.redirect(302, `${CLIENT_BASE_URL}/profile/complete`);
   }
 };
