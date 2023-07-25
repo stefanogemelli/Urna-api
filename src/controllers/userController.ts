c5db857088da9442f467c6f6c0ad2f916be93967
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-07-25 15:17:30 -0300

    refactor: createUser

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -7,8 +7,8 @@ export const getUsers = async (req, res) => {
 };
 
 export const createUser = async (req, res) => {
-  const userToCreate = req.body;
-  const newUser = await User.create(userToCreate);
+  const { userProfile } = req.body;
+  const newUser = await User.create(userProfile);
   response(res, 201, newUser);
 };
 
