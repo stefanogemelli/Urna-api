443e3d111b2d15fac672a1a29dfc4d204ae6b281
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-07-25 11:00:45 -0300

    perf: add accessChecker in post: /voting

--- src/routes/votingRouter.ts
+++ src/routes/votingRouter.ts
@@ -2,13 +2,14 @@ import Router from "express";
 import { Voting } from "../controllers";
 import { catchedAsync } from "../utils";
 import { votingValidation } from "../middlewares/validations";
+import { accessRoleChecker } from "../middlewares/authorization";
 
 const router = Router();
 
 // recibe por query la prop "verified" = true/false
 router.get("/", catchedAsync(Voting.getAll));
 
-router.post("/", votingValidation.create, catchedAsync(Voting.create));
+router.post("/", accessRoleChecker("user"), votingValidation.create, catchedAsync(Voting.create));
 
 // router.patch("/:id", userValidation.update, catchedAsync(User.updateUser));
 
