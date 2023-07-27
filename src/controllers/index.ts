172e1fc80f1e98b33a8e1af613f5bb82c3467837
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-07-27 17:55:39 -0300

    feat: add option controller

--- src/controllers/index.ts
+++ src/controllers/index.ts
@@ -1,4 +1,5 @@
 export * as User from "./userController";
 export * as Region from "./regionController";
 export * as Voting from "./votingController";
+export * as Option from "./optionController";
 export * as Authentication from "./authenticationController";
