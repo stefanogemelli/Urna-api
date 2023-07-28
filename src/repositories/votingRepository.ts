ade502fe63262672e9e27ad83b0249a4de00fe10
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-07-28 16:46:48 -0300

    fix: voting creation works

--- src/repositories/votingRepository.ts
+++ src/repositories/votingRepository.ts
@@ -3,8 +3,8 @@ import { Voting } from "../database/schemas";
 export const VotingRepository = {
   verifiedList: async () => await Voting.verifiedList(),
   listToVerify: async () => await Voting.listToVerify(),
-  create: async (voting) => Voting.create(voting),
+  create: async (newVotingData) => Voting.insertNew(newVotingData),
   update: async (voting) => Voting.update(voting),
-  getById: async (id:string) => Voting.getById(id)
+  getById: async (id: string) => Voting.getById(id),
   // delete: async (id) => Voting.delete(id),
 };
