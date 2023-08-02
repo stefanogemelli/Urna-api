118f39eb8fb4e8ca302c7a22256a22adfbf8ce69
FrancoFrias23 <lockke2323@gmail.com>
2023-08-02 16:58:56 -0300

    refactor: delete comment changes responseSchema

--- src/database/schemas/responsesSchema.ts
+++ src/database/schemas/responsesSchema.ts
@@ -4,7 +4,7 @@ import { conn } from "../connection";
 
 export interface IResponse {
   _id: string;
-  comment_id: string;
+  vote_id: string;
   emitter_id: string;
   receiver_id: string;
   body: string;
@@ -12,7 +12,9 @@ export interface IResponse {
   deleted: boolean;
 }
 
-export interface ResponseModel extends Model<IResponse> {}
+export interface ResponseModel extends Model<IResponse> {
+  getByVoteId(id: string): Array<IResponse>;
+}
 
 const responseSchema = new Schema<IResponse, ResponseModel>(
   {
@@ -20,12 +22,12 @@ const responseSchema = new Schema<IResponse, ResponseModel>(
       type: String,
       default: () => uuidv4(),
     },
-    comment_id: {
+    vote_id: {
       type: String,
       required: true,
       unique: true,
-      validate: (commentId: string) => validateUUID(commentId),
-      ref: "Comment",
+      validate: (voteId: string) => validateUUID(voteId),
+      ref: "Vote",
     },
     emitter_id: {
       type: String,
@@ -57,5 +59,8 @@ const responseSchema = new Schema<IResponse, ResponseModel>(
   },
   { timestamps: true }
 );
+responseSchema.statics.getByVoteId = async function (vote_id) {
+  return await this.find({vote_id});
+};
 
 export const Response = conn.model<IResponse, ResponseModel>("Response", responseSchema);
