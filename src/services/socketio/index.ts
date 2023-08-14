522ce78da5d5f2217d73e6924644054a3ce8a067
Stefano Gemelli <stefanogemelli06@gmail.com>
2023-08-14 13:16:29 -0300

    feat: add room methods (join&leave)

--- src/services/socketio/index.ts
+++ src/services/socketio/index.ts
@@ -1,15 +1,24 @@
 export default function onConnection(io) {
   return function (socket) {
-    console.log(socket.id);
-
-    socket.on("joinRoom", ({ room, username }, cb) => {
-      socket.join(room);
-      cb("mensaje para el log del cb");
-      socket.broadcast.emit("joinRoom_response", username + " se unión a la room " + room);
+    console.log("Connect", socket.id);
+    socket.on("disconnect", () => {
+      console.log("Disconnect " + socket.id);
     });
 
+    const joinRoom = ({ roomType, roomId }) => {
+      socket.join(`room:${roomType}:${roomId}`);
+      console.log(`${socket.id} se unio a la sala: room:${roomType}:${roomId}`);
+    };
+    const leaveRoom = ({ roomType, roomId }, cb) => {
+      socket.leave(`room:${roomType}:${roomId}`);
+      console.log(`${socket.id} abandonó la sala: room:${roomType}:${roomId}`);
+    };
+
+    socket.on("room:join", joinRoom);
+    socket.on("room:leave", leaveRoom);
+
     socket.on("message", (msg) => {
-      socket.broadcast.emit("messageFromServer", msg);
+      socket.broadcast.emit("server:message", msg);
     });
   };
 }
